8cecdabf9cdfe33b5647c4cac227b53b6bfaceb1
gil-koifman <gikoifma@microsoft.com>
2023-01-03 09:13:12 +0200

    add comment

--- package/src/accessibilityUtils.ts
+++ package/src/accessibilityUtils.ts
@@ -5,6 +5,10 @@ export function runAccessibilityFixers() {
     fixCommandPalleteHeight();
 }
 
+/**
+ * Fixing command pallete completion list not showing with small height.
+ * Fix is from here: https://github.com/microsoft/monaco-editor/issues/70
+ */
 function fixCommandPalleteHeight() {
     let module = require("vs/base/parts/quickinput/browser/quickInputList");
     module.QuickInputList.prototype.layout = function (maxHeight: number) {
