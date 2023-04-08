480ee3da431330a4478b7f83ce8cf31ad932723f
gil-koifman <gikoifma@microsoft.com>
2023-04-08 16:59:44 +0300

    Make sure options with more words, come after shorter options

--- package/src/languageService/kustoLanguageService.ts
+++ package/src/languageService/kustoLanguageService.ts
@@ -31,7 +31,7 @@ class ParseProperties {
         private uri: string,
         private rulesProvider?: k.IntelliSenseRulesProviderBase,
         private parseMode?: k.ParseMode
-    ) {}
+    ) { }
 
     isParseNeeded(document: TextDocument, rulesProvider?: k.IntelliSenseRulesProviderBase, parseMode?: k.ParseMode) {
         if (
@@ -437,19 +437,23 @@ class KustoLanguageService implements LanguageService {
                 const { textToInsert, format } =
                     kItem.AfterText && kItem.AfterText.length > 0
                         ? {
-                              // Need to escape dollar sign since it is used as a placeholder in snippet.
-                              // Usually dollar sign is not a valid character in a function name, but grafana uses macros that start with dollars.
-                              textToInsert: `${kItem.EditText.replace('$', '\\$')}$0${kItem.AfterText}`,
-                              format: ls.InsertTextFormat.Snippet,
-                          }
+                            // Need to escape dollar sign since it is used as a placeholder in snippet.
+                            // Usually dollar sign is not a valid character in a function name, but grafana uses macros that start with dollars.
+                            textToInsert: `${kItem.EditText.replace('$', '\\$')}$0${kItem.AfterText}`,
+                            format: ls.InsertTextFormat.Snippet,
+                        }
                         : {
-                              textToInsert: kItem.EditText,
-                              format: ls.InsertTextFormat.PlainText,
-                          };
+                            textToInsert: kItem.EditText,
+                            format: ls.InsertTextFormat.PlainText,
+                        };
                 const lsItem = ls.CompletionItem.create(kItem.DisplayText);
 
                 // Adding to columns a prefix to their sortText so they will appear first in the list
-                const sortTextPrefix = lsItem.kind === ls.CompletionItemKind.Field ? 0 : itemsAsArray.length;
+                let sortTextPrefix = lsItem.kind === ls.CompletionItemKind.Field ? 0 : itemsAsArray.length;
+                const countOfWords = lsItem.label.split("_").length;
+                // We always want results with multiple words to show after shorter options.
+                // For example: to show "count_distinct" after "count"
+                sortTextPrefix = sortTextPrefix * countOfWords;
                 const startPosition = document.positionAt(completionItems.EditStart);
                 const endPosition = document.positionAt(completionItems.EditStart + completionItems.EditLength);
                 lsItem.textEdit = ls.TextEdit.replace(ls.Range.create(startPosition, endPosition), textToInsert);
@@ -888,14 +892,14 @@ class KustoLanguageService implements LanguageService {
                     // a command is affected if it intersects at least on of changed ranges.
                     command // command can be null. we're filtering all nulls in the array.
                         ? changeIntervals.some(
-                              ({ start: changeStart, end: changeEnd }) =>
-                                  // both intervals intersect if either the start or the end of interval A is inside interval B.
-                                  // If we deleted something at the end of a command, the interval will not intersect the current command.
-                                  // so we also want consider affected commands commands the end where the interval begins.
-                                  // hence the + 1.
-                                  (command.AbsoluteStart >= changeStart && command.AbsoluteStart <= changeEnd) ||
-                                  (changeStart >= command.AbsoluteStart && changeStart <= command.AbsoluteEnd + 1)
-                          )
+                            ({ start: changeStart, end: changeEnd }) =>
+                                // both intervals intersect if either the start or the end of interval A is inside interval B.
+                                // If we deleted something at the end of a command, the interval will not intersect the current command.
+                                // so we also want consider affected commands commands the end where the interval begins.
+                                // hence the + 1.
+                                (command.AbsoluteStart >= changeStart && command.AbsoluteStart <= changeEnd) ||
+                                (changeStart >= command.AbsoluteStart && changeStart <= command.AbsoluteEnd + 1)
+                        )
                         : false
                 );
 
@@ -980,11 +984,11 @@ class KustoLanguageService implements LanguageService {
             // a command is affected if it intersects at least on of changed ranges.
             block // command can be null. we're filtering all nulls in the array.
                 ? changeIntervals.some(
-                      ({ start: changeStart, end: changeEnd }) =>
-                          // both intervals intersect if either the start or the end of interval A is inside interval B.
-                          (block.Start >= changeStart && block.Start <= changeEnd) ||
-                          (changeStart >= block.Start && changeStart <= block.End + 1)
-                  )
+                    ({ start: changeStart, end: changeEnd }) =>
+                        // both intervals intersect if either the start or the end of interval A is inside interval B.
+                        (block.Start >= changeStart && block.Start <= changeEnd) ||
+                        (changeStart >= block.Start && changeStart <= block.End + 1)
+                )
                 : false
         );
     }
@@ -1149,10 +1153,10 @@ class KustoLanguageService implements LanguageService {
                                 cslDefaultValue: inputParam.CslDefaultValue,
                                 columns: inputParam.Columns
                                     ? inputParam.Columns.map((col) => ({
-                                          name: col.Name,
-                                          type: col.Type,
-                                          cslType: col.CslType,
-                                      }))
+                                        name: col.Name,
+                                        type: col.Type,
+                                        cslType: col.CslType,
+                                    }))
                                     : (inputParam.Columns as undefined | null | []),
                             })),
                         })),
@@ -1783,7 +1787,7 @@ class KustoLanguageService implements LanguageService {
     }
     //#endregion
 
-    private static convertToEntityDataType(kustoType: string) {}
+    private static convertToEntityDataType(kustoType: string) { }
     /**
      * We do not want to expose Bridge.Net generated schema, so we expose a cleaner javascript schema.
      * Here it gets converted to the bridge.Net schema
@@ -2314,23 +2318,23 @@ class KustoLanguageService implements LanguageService {
             this._rulesProvider =
                 this._languageSettings && this._languageSettings.includeControlCommands
                     ? new k.CslIntelliSenseRulesProvider.$ctor1(
-                          engineSchema.Cluster,
-                          engineSchema,
-                          queryParameters,
-                          availableClusters,
-                          null,
-                          true,
-                          true
-                      )
+                        engineSchema.Cluster,
+                        engineSchema,
+                        queryParameters,
+                        availableClusters,
+                        null,
+                        true,
+                        true
+                    )
                     : new k.CslQueryIntelliSenseRulesProvider.$ctor1(
-                          engineSchema.Cluster,
-                          engineSchema,
-                          queryParameters,
-                          availableClusters,
-                          null,
-                          null,
-                          null
-                      );
+                        engineSchema.Cluster,
+                        engineSchema,
+                        queryParameters,
+                        availableClusters,
+                        null,
+                        null,
+                        null
+                    );
             return;
         }
 
