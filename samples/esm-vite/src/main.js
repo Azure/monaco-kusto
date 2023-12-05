1bc93ae9664f49d8f8686bb0c90866697904a2e3
Seth Reid <contact@sethreid.co.nz>
2023-12-05 15:01:34 +1300

    Add vite-react sample project (#391)
    ### Summary
    
    This PR aims to add an example of using this package in a project that
    is built using vite and react.
    
    > Note: This is not yet working and I am seeking help from the
    maintainers of the package to figure out how.
    
    #### How to run
    
    In the foloer /samples/vite-react:
    
    ```
    yarn install
    yarn dev
    ```
    
    ---------
    
    Co-authored-by: Maxwell Burson <maburson@microsoft.com>

---
+++ samples/esm-vite/src/main.js
@@ -0,0 +1,74 @@
+import * as monaco from 'monaco-editor/esm/vs/editor/edcore.main';
+import { getKustoWorker } from '@kusto/monaco-kusto';
+import './index.css';
+
+// Vite doesn't let us directly import files in dependencies as url's for some
+// reason. Instead, we'll import local files as url's, and they'll import what
+// we really want.
+import kustoWorkerUrl from './kustoWorker?url';
+import editorWorker from './editorWorker?url';
+
+window.MonacoEnvironment = {
+    getWorker(_moduleId, label) {
+        switch (label) {
+            case 'kusto':
+                return new Worker(kustoWorkerUrl, { type: 'module' });
+            default:
+                return new Worker(editorWorker, { type: 'module' });
+        }
+    },
+};
+
+// Called by playwright script in ci to validate things are working
+window.healthCheck = async function () {
+    return !!(await getKustoWorker());
+};
+
+const schema = {
+    Plugins: [],
+    Databases: {
+        Samples: {
+            Name: 'Samples',
+            Tables: {
+                StormEvents: {
+                    Name: 'StormEvents',
+                    DocString:
+                        'A dummy description to test that docstring shows as expected when hovering over a table',
+                    OrderedColumns: [
+                        {
+                            Name: 'StartTime',
+                            Type: 'System.DateTime',
+                            CslType: 'datetime',
+                            DocString: 'The start time',
+                        },
+                        {
+                            Name: 'State',
+                            Type: 'System.String',
+                            CslType: 'string',
+                        },
+                    ],
+                },
+            },
+            Functions: {},
+        },
+    },
+};
+
+const editor = monaco.editor.create(document.getElementById('root'), {
+    value: 'StormEvents | take 10',
+    language: 'kusto',
+    theme: 'kusto-light',
+});
+
+window.addEventListener('resize', () => {
+    editor.layout();
+});
+
+getKustoWorker().then((workerAccessor) => {
+    const model = editor.getModel();
+    if (model) {
+        workerAccessor(model.uri).then((worker) => {
+            worker.setSchemaFromShowSchema(schema, 'https://help.kusto.windows.net', 'Samples');
+        });
+    }
+});
---
+++ samples/esm-vite/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    // nodePolyfills required because of @kusto/language-service
+    plugins: [nodePolyfills({ overrides: { fs: null } })],
+    optimizeDeps: {
+        include: [
+            // https://github.com/vitejs/vite/discussions/1791#discussioncomment-320938
+            // add more modules here when you get a "new dependencies optimized" warning from vite.
+            'monaco-editor/esm/vs/language/json/json.worker',
+            '@kusto/monaco-kusto/release/esm/kusto.worker',
+            '@kusto/language-service/bridge.min',
+            'monaco-editor/esm/vs/editor/editor.worker',
+            'xregexp',
+        ],
+    },
+});
