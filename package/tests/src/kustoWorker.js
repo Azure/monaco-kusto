334ae3dfe0f2a13ba36c7959f953897df81c015a
morgilad <morgilad86@gmail.com>
2024-05-14 11:52:29 +0300

    add vite dev env

---
+++ package/tests/src/kustoWorker.js
@@ -0,0 +1,7 @@
+// The next import is needed to preload `bridge.min.js` because if I don't, some
+// weird Vite-stuff makes it load after `Kusto.JavaScript.Client.min` (in
+// `kusto.worker.js`), which causes the `Bridge` object to be undefined. Only
+// necessary for Vite.
+import '@kusto/language-service/bridge.min';
+
+import '@kusto/monaco-kusto/release/esm/kusto.worker';
