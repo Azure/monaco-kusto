2bb7d3d820d2c1ed1f2b03b60698ab793b3e8f86
morgilad <morgilad86@gmail.com>
2024-05-16 10:34:49 +0300

    exit with failure when integration tests fails

--- package/scripts/test.ts
+++ package/scripts/test.ts
@@ -1,33 +1,14 @@
 import concurrently from 'concurrently';
 import { packageFolder } from './lib';
-import { argv } from 'process';
-import { execSync } from 'child_process';
 
 async function main() {
-    const watch = argv.includes('--watch');
+    const build = 'yarn rollup -c ./scripts/rollup.esm.js -w --bundleConfigAsCjs';
+    const tests = 'yarn playwright test --ui --headed --trace=on -c tests/playwright.config.ts';
 
-    const buildOnce = 'yarn rollup -c ./scripts/rollup.esm.js --bundleConfigAsCjs';
-    const buildWatch = 'yarn rollup -c ./scripts/rollup.esm.js -w --bundleConfigAsCjs';
-    const build = watch ? buildWatch : buildOnce;
-
-    const testsRun = 'yarn playwright test --trace=retain-on-failure -c tests/playwright.config.ts';
-    const testsWatch = 'yarn playwright test --ui --headed --trace=on -c tests/playwright.config.ts';
-    const tests = watch ? testsWatch : testsRun;
-
-    if (watch) {
-        console.log('Running build and tests in watch mode');
-        concurrently([build, tests], {
-            cwd: packageFolder,
-        });
-    } else {
-        console.log('Running build');
-        execSync(build, { cwd: packageFolder, stdio: 'inherit' });
-        console.log('Build completed');
-
-        console.log('Running tests');
-        execSync(tests, { cwd: packageFolder, stdio: 'inherit' });
-        console.log('Tests completed');
-    }
+    console.log('Running build and tests in watch mode');
+    concurrently([build, tests], {
+        cwd: packageFolder,
+    });
 }
 
 main();
--- package/tests/completion-items.spec.ts
+++ package/tests/completion-items.spec.ts
@@ -6,11 +6,9 @@ test('trigger completion on "("', async ({ page }) => {
     await page.goto('http://localhost:7777/');
 
     const value = `${initialValue} \n| where StartTime > ago`;
-
     await page.getByRole('textbox').fill(value);
     await page.keyboard.type('(');
 
     const option = page.getByRole('option').first();
-
     await expect(option).toHaveText('1d');
 });
