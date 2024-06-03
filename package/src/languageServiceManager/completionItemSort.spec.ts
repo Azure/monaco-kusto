846f8073a6645684195ad0c47ddd5e6bfa01b56b
morgilad <morgilad86@gmail.com>
2024-06-03 14:24:53 +0300

    change unit test

--- package/src/languageServiceManager/completionItemSort.spec.ts
+++ package/src/languageServiceManager/completionItemSort.spec.ts
@@ -3,11 +3,24 @@ import { createSortingText } from './competionItemSort';
 
 describe('createSortingText', () => {
     test('returns the correct sort text according to the given order', () => {
-        expect(createSortingText(0)).toBe('0000000000');
-        expect(createSortingText(1)).toBe('0000000001');
-        expect(createSortingText(25)).toBe('0000000025');
-        expect(createSortingText(26)).toBe('0000000026');
-        expect(createSortingText(51)).toBe('0000000051');
-        expect(createSortingText(123)).toBe('0000000123');
+        const items = [
+            { priority: 51, label: 'fifth' },
+            { priority: 26, label: 'forth' },
+            { priority: 0, label: 'first' },
+            { priority: 123, label: 'sixth' },
+            { priority: 1, label: 'second' },
+            { priority: 25, label: 'third' },
+        ];
+
+        const sortedItems = items.sort((a, b) =>
+            createSortingText(a.priority).localeCompare(createSortingText(b.priority))
+        );
+
+        expect(sortedItems[0].label).toBe('first');
+        expect(sortedItems[1].label).toBe('second');
+        expect(sortedItems[2].label).toBe('third');
+        expect(sortedItems[3].label).toBe('forth');
+        expect(sortedItems[4].label).toBe('fifth');
+        expect(sortedItems[5].label).toBe('sixth');
     });
 });
