4cace055494e20a4e88e11761c2224c37107dd7e
Mor <morgilad@users.noreply.github.com>
2024-06-05 12:23:24 +0300

    chore: fix local vite env (#462)

--- package/scripts/dev.ts
+++ package/scripts/dev.ts
@@ -2,8 +2,8 @@ import concurrently from 'concurrently';
 
 import { packageFolder } from './lib';
 
-async function main() {
-    const server = 'vite tests serve';
+function main() {
+    const server = 'vite tests/integration serve --config ./tests/integration/vite.config.ts';
     const build = 'yarn rollup -c ./scripts/rollup.esm.js -w --bundleConfigAsCjs';
 
     concurrently([server, build], {
