e3f0985717a41d866e10f37cd87369cf0272df14
morgilad <morgilad86@gmail.com>
2024-07-25 18:56:05 +0300

    add empty line to remove diff

--- package/src/syntaxHighlighting/SemanticTokensProvider.ts
+++ package/src/syntaxHighlighting/SemanticTokensProvider.ts
@@ -22,6 +22,7 @@ export class SemanticTokensProvider implements monaco.languages.DocumentSemantic
             const previousClassification = classifications[index - 1];
             return semanticTokenMaker(classification, previousClassification);
         });
+
         return {
             data: new Uint32Array(semanticTokens.flat()),
             resultId: model.getVersionId().toString(),
