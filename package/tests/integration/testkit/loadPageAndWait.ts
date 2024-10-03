e2e83e774ffb0fe57a3676b50673bae2c1a3e19c
Maxwell Burson <maburson@microsoft.com>
2024-10-02 17:39:11 -0700

    Ugggg

--- package/tests/integration/testkit/loadPageAndWait.ts
+++ package/tests/integration/testkit/loadPageAndWait.ts
@@ -2,5 +2,5 @@ import { Page } from '@playwright/test';
 
 export async function loadPageAndWait(page: Page) {
     await page.goto('http://localhost:7777/');
-    await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('networkidle', { timeout: 30_000 });
 }
