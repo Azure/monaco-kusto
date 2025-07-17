69b1a52c68e4d96995277d465c1dad42e3b57397
Maxwell Burson <maburson@microsoft.com>
2025-07-16 19:44:41 -0700

    Bump version number

--- package/src/languageServiceManager/kustoLanguageService.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, describe, expect } from '@jest/globals';
-
-describe('getReferencedGlobalParams', () => {
-    test('maximumDepthExceeded', () => {
-        const languageService = KustoLanguageService;
-    });
-});
